5b833a70374e7337620894e2df3c928d08577ae5
rajshiwank <111116080+rajshiwank@users.noreply.github.com>
2022-08-12 13:54:13 +0530

    Add initals

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("car-music").onclick = function() {
+//     document.getElementById("my_audio").play();
+// }
+
+window.onload = function() {
+    document.getElementById("my_audio").play();
+}
